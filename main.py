1587b377ac00d9f351cba7de91410a12e47def53
Chitaoji <2360742040@qq.com>
2024-10-22 15:49:48 +0800

    Update main.py

--- main.py
+++ main.py
@@ -14,23 +14,21 @@ If you want to import from the module, use the main `readpub` namespace instead.
 import argparse
 from typing import TYPE_CHECKING
 
-if TYPE_CHECKING:
-    from .src.kivyapp import KivyApp
-else:
-    from src.kivyapp import KivyApp
-
-parser = argparse.ArgumentParser(description="works as the launcher for readpub.")
-parser.add_argument(
-    "-f",
-    "--frame",
-    help="choose the gui frame (default: %(default)s)",
-    default="kivy",
-    choices=["kivy"],
+parser = argparse.ArgumentParser(
+    prog="readpub/main.py", description="Launch a readpub application."
 )
-
+subparsers = parser.add_subparsers(
+    title="choose the gui frame (default: kivy)", dest="subcommand"
+)
+subparsers.add_parser("kivy", help="Kivy/KivyMD")
 
 if __name__ == "__main__":
     args = parser.parse_args()
-    match args.frame:
-        case "kivy":
+    match args.subcommand:
+        case "kivy" | None:
+            if TYPE_CHECKING:
+                from .src.kivyapp import KivyApp
+            else:
+                from src.kivyapp import KivyApp
+
             KivyApp().run()
